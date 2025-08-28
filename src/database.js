44886621b46dda0c1f0a915a083d9df761af921a
24DavidM <stalin.muela02@epn.edu.ec>
2025-08-28 08:51:49 -0500

    cambio

--- src/database.js
+++ src/database.js
@@ -2,13 +2,18 @@ import mongoose from 'mongoose'
 
 mongoose.set('strictQuery', true)
 
-const connection = async() =>{
+const connection = async () => {
+    const uri = process.env.MONGO_URI_LOCAL;
+    if (!uri) {
+        console.error("La variable de entorno MONGO_URI_LOCAL no está definida. Verifica tu archivo .env");
+        process.exit(1);
+    }
     try {
-        const {connection} = await mongoose.connect(process.env.MONGO_URI_LOCAL)
-        console.log(`Database is connected on ${connection.host} - ${connection.port}`)
+        const { connection } = await mongoose.connect(uri);
+        console.log(`Database is connected on ${connection.host} - ${connection.port}`);
     } catch (error) {
-        console.log("Eror al conectar",error)
-        process.exit(1)
+        console.log("Eror al conectar", error);
+        process.exit(1);
     }
 }
 
